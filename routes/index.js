26cc5b8aa3d1c6d135c78ca87c54baeaecb6caef
Eric Denton <eric@iamericanartist.com>
2016-10-01 16:18:38 -0500

    updated port, user unique, and minor things

--- routes/index.js
+++ routes/index.js
@@ -4,25 +4,30 @@ const { Router } = require('express')
 const router = Router()
 // const bcrypt = require("bcrypt")
 
+/////////////////////////////////  USER MODEL  /////////////////////////////////
 const User = require('../models/user')
 
 
-
+/////////////////////////////////  HOME ROUTE  /////////////////////////////////
 router.get('/', function (req, res) {
   // res.send('Hello World!')
   res.render("home")
 })
 
 
+////////////////////////////////  LOGIN ROUTES  ////////////////////////////////
 router.get('/login', function (req, res) {
-  res.render("login")
+  res.render('login')
 })
 
-router.get('/logout', function (req, res) {
+
+////////////////////////////////  LOGOUT ROUTES  ////////////////////////////////
+router.post('/logout', function (req, res) {
   res.render("logout")
 })
 
 
+///////////////////////////////  REGISTER ROUTES  ///////////////////////////////
 router.get('/register', function (req, res) {
   res.render("register")
 })
@@ -30,7 +35,9 @@ router.post('/register', function (req, res) {
   console.log("req.body", req.body)
   User
     .create({ user: req.body.user, pass: req.body.pass })
-    .then(() => res.send("I have no idea."))
+    // .then(() => res.send("I have no idea."))
+    .then(() => res.redirect('/login'))
+    .catch(err)
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -11,26 +11,31 @@ const { connect } = require("./db/database")
 // const session = require('express-session')
 // const RedisStore = require('connect-redis')(session)  //grabbing from line above and adding it here
 
-// USING PASSPORT AUTH
-// const passport = require("passport")
 // USING mLAB DATABASE
 
 const port = process.env.PORT || 3000
 app.set("port", port)
 
 app.set("view engine", "pug")
+
+/////////////////////////////////  MIDDLEWARES  /////////////////////////////////
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
 
-///////////////////////////////////  Other  ///////////////////////////////////
+////////////////////////////////////  Other  ////////////////////////////////////
 app.locals.company = "Loginr"
 
+
+///////////////////////////////////  Routes  ///////////////////////////////////
 app.use(routes)
 
 
+// connect to database an initiate with port defined above
 connect()
-.then(() => {
-  app.listen(3000, function () {
-    console.log('Loginr app listening on port', port);
+  .then(() => {
+    // change port here (from 3000) to use template literal ${port} in conlog below
+    app.listen(port, () => {
+      console.log(`Loginr app listening on port', ${port}`) 
+    })
   })
-})
+  .catch(console.error)
